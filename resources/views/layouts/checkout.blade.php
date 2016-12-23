c8d0de5a32f7b48d3815e023308ace9563de2aa5
JohanArmando <johan.maxcorp@gmail.com>
2016-12-23 14:01:55 +0000

    checkout tildes

--- resources/views/layouts/checkout.blade.php
+++ resources/views/layouts/checkout.blade.php
@@ -1,8 +1,7 @@
 <!doctype html>
-<html lang="es" ng-app="checkout">
+<html lang="en">
 <head>
    <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
    <title>Checkout</title>
 
    <link href="{{ asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
