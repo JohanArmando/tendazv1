e6db894d848fd8b800f758cb2e1b0f515fab0308
JohanArmando <johan.maxcorp@gmail.com>
2016-12-23 13:58:01 +0000

    checkout tildes

--- resources/views/layouts/checkout.blade.php
+++ resources/views/layouts/checkout.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="es" ng-app="checkout">
 <head>
-   <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+   <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
    <title>Checkout</title>
 
