f33830fa3b0a8afe061a5bf032433531b72f8d6e
JohanArmando <johan.maxcorp@gmail.com>
2016-12-27 15:30:08 -0500

    logos

--- public/new-checkout/js/controllers.js
+++ public/new-checkout/js/controllers.js
@@ -84,21 +84,7 @@ myApp.controller('globalController' , ["$scope" , "User", "$rootScope" , "$locat
 }]);
 myApp.controller("navController" , ["$scope" , "User" , "$location" ,  function ($scope , User , $location ) {
     angular.extend($scope , {
-    	       return $http({
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            url: baseUrl + '/carts/' + cartId + '/items?client_secret='  + client_secret + '&client_id=' + client_id,
-            method: "GET",
-        }).then(function(response) {
-           $rootScope.carts = response.data.data;
-           $rootScope.shopData = $rootScope.carts.shop.data;
-           console.log( $rootScope.shopData);
-        }).catch(function(response) {
-            console.log(response);
-        }).finally(function() {});
-    };
+    	
        doLogout : function () {
            User.doUserLogout();
            $location.path('/cart');
