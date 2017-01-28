4a9f947cd1f254bf542f0e66874ec68cc1459104
johinsDev <johinsdev@gmail.com>
2017-01-28 15:00:23 +0000

    subscriptionmiddleware

--- app/Http/Middleware/SubscriptionMiddleware.php
+++ app/Http/Middleware/SubscriptionMiddleware.php
@@ -18,7 +18,7 @@ class SubscriptionMiddleware
     {
         if (isset($request->route()->getAction()['notMiddleware']) && $request->route()->getAction()['notMiddleware'] == 'subscription')
             return $next($request);
-        if(!$request->shop->subscription()){
+        if(!$request->shop->subscription_id){
             $request->shop->makeSubscription(Plan::first());
         }
         if ($request->shop->subscription()->subscribed() || $request->shop->subscription()->onTrial() || $request->shop->subscription()->onGracePeriod())
