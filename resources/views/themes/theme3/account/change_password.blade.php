4eddfeaa82c06bbe0da55067e22dcce60f419e7b
JuanMa12 <jumaru-97@hotmail.com>
2017-02-28 17:07:53 -0500

    change in updatepassword

--- resources/views/themes/theme3/account/change_password.blade.php
+++ resources/views/themes/theme3/account/change_password.blade.php
@@ -48,7 +48,7 @@
 											<div class="row">
 												<div class="text-center">
 													<button type="submit" class="btn btn--ys btn-primary btn-top btn--xl changePasswordButton" >
-														<span class="icon icon-check"></span>Actualizar</button>
+														<span class="icon icon-check"></span>Actualizar Contrase&ntilde;a</button>
 												</div>
 												<div class="divider divider--md visible-xs"></div>
 											</div>
