b6b3dcffaded9f47c0dd3f47a09ec1a30d6f8cb8
JuanMa12 <jumaru-97@hotmail.com>
2017-03-07 15:02:05 -0500

    change api key

--- config/nameCheap.php
+++ config/nameCheap.php
@@ -8,7 +8,7 @@
 
 return [
     'NAMECHEAP' => [
-        'api_key' => env('name_cheap_api_key' , 'd746e92d21d24a46a194c6535a5d24a3'),
+        'api_key' => env('name_cheap_api_key' , '42ca3f56ff21490986b58fc09f3c4c3b'),
         'api_user' => env('name_cheap_api_user' , 'sectorstream'),
         'url' => env('url_name_cheap' , 'https://api.namecheap.com/xml.response'),
     ],
